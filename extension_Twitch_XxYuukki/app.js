3c339063d054821e2bdae6be1c711b3599c6117b
HippopoStar <leocabanes@wanadoo.fr>
2018-11-23 21:11:44 +0100

    Ajout ebauche d'extension Twitch

--- extension_Twitch_XxYuukki/app.js
+++ extension_Twitch_XxYuukki/app.js
@@ -1,11 +1,11 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.twitch.tv/helix/streams?user_id=97552678", true);
-xhr.setRequestHeader("Client-ID: [...]");
+xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=xxyuukki", true);
+xhr.setRequestHeader("Client-ID","[...]");
 xhr.onreadystatechange = function(){
 	if (xhr.readyState == 4){
 		var data = JSON.parse(xhr.responseText);
 		var elm = document.getElementById("info")
-		if (data["data"] == []){
+		if (data["data"].length == 0){
 			elm.style.color = "red"
 			elm.innerHTML = "XxYuukki est actuellement hors ligne"
 		}else{
--- extension_Twitch_XxYuukki/background.js
+++ extension_Twitch_XxYuukki/background.js
@@ -6,12 +6,12 @@ setInterval(function(){
 
 function checkStream(){
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://api.twitch.tv/kraken/streams?user_id=97552678", true);
-	xhr.setRequestHeader("Client-ID: [...]");
+	xhr.open("GET", "https://api.twitch.tv/kraken/streams?user_login=xxyuukki", true);
+	xhr.setRequestHeader("Client-ID","[...]");
 	xhr.onreadystatechange = function(){
 		if (xhr.readyState == 4){
 			var data = JSON.parse(xhr.responseText);
-			if (data["data"] == []){
+			if (data["data"].length == 0){
 				chrome.browserAction.setIcon({path: "img/icon_red.png"})
 			}else{
 				chrome.browserAction.setIcon({path: "img/icon_green.png"})
